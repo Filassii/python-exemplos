dd41eb51a31f7c6b82503a51d04f281c12a18075
Filassii <mateusfilassii@gmail.com>
2024-04-23 16:37:41 -0300

    criação variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,12 @@
+nome="Mateus Lopes Filassi"
+idade=16
+email='mateuslopesfilassi@gmail.com'
+telefone="17996492038"
+nota=7
+aprovado=True
+print(nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovado)
