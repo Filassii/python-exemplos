5121a32ce094ade08f5f1c3d7a9990c0ad4098ce
Filassii <mateusfilassii@gmail.com>
2024-05-21 14:52:52 -0300

    Create listas.py

---
+++ listas.py
@@ -0,0 +1,8 @@
+aluno = ["Mateus Filassi" , 4, 8.5, True]
+print(f"Nome: {aluno[0]}")
+print(f"Faltas: {aluno[1]}")
+print(f"Nota: {aluno[2]}")
+if aluno[3] == True:
+    print(f"Aprovado: Sim")
+else:
+    print(f"Aprovado: Não")
