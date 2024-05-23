b73ed11c79236436cd442343a17378c2a060a504
Filassii <mateusfilassii@gmail.com>
2024-05-23 10:08:05 -0300

    Create lista_clonar.py

---
+++ lista_clonar.py
@@ -0,0 +1,6 @@
+alunos =['Mateus', 'joão', 'Gabriel', 'Felipe', 'Ana Clara', 'Ana Beatriz']
+copia=list(alunos)
+print(f'Lista original: {alunos}')
+print(f'Lista Cópia: {copia}')
+copia.remove('Gabriel')
+print(f'Lista Cópia: {copia}')
