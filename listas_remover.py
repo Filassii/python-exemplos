03dfb1a23d490b078f652b35b7e6c54e35666e2d
Filassii <mateusfilassii@gmail.com>
2024-05-23 09:28:37 -0300

    Create listas_remover.py

---
+++ listas_remover.py
@@ -0,0 +1,13 @@
+alunos=['Mateus', 'Ana clara', 'Gabriel', 'Rafael', 'Ana Beatriz', 'Kaiky', 'Andrade', 'Mateus']
+print(f'Listas de alunos: {alunos}')
+#Método remove()
+alunos.remove('Mateus')
+print(f'Lista de alunos: {alunos}')
+#Método pop()
+alunos.pop(2)
+print(f'Listas de alunos: {alunos}')
+#Função del
+del alunos[3]
+print(f'Listas de alunos: {alunos}')
+#Método clear()
+print(f'Listas de alunos: {alunos}')
