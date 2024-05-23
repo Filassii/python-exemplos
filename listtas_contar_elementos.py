a7615453eb957b7582d6a909ece81204ef767278
Filassii <mateusfilassii@gmail.com>
2024-05-23 10:41:12 -0300

    Create listtas_contar_elementos.py

---
+++ listtas_contar_elementos.py
@@ -0,0 +1,3 @@
+alunos=['Mateus', 'Felipe', 'Gabriel', 'Rafael', 'Ana CLara', 'Ana Beatriz', 'Lara', 'Mateus']
+ocorrencias=alunos.count('Mateus')
+print(f'Quantidade de ocorrencias do Mateus: {ocorrencias}')
